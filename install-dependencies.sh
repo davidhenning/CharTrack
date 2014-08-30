1003c69862189fa4c4fb47c9c763782ff2c9ed1f
David <david@webcodr.de>
2014-08-31 00:57:50 +0200

    Use Android SDK 19 for Circle CI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -7,11 +7,11 @@ DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
   cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
-  echo y | android update sdk -u -a -t android-18 &&
+  echo y | android update sdk -u -a -t android-19 &&
   echo y | android update sdk -u -a -t platform-tools &&
-  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
-  echo y | android update sdk -u -a -t sys-img-x86-android-18 &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
-  echo n | android create avd -n testing -f -t android-18 &&
+  echo y | android update sdk -u -a -t build-tools-19.1.0 &&
+  echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
+  echo n | android create avd -n testing -f -t android-19 &&
   touch $DEPS
 fi
