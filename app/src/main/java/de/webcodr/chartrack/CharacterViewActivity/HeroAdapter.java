7c6a310e62e589b4aca0bfc79aed87230d13399c
David <david@webcodr.de>
2014-08-31 17:36:50 +0200

    Use View Holder pattern for better performance

--- app/src/main/java/de/webcodr/chartrack/CharacterViewActivity/HeroAdapter.java
+++ app/src/main/java/de/webcodr/chartrack/CharacterViewActivity/HeroAdapter.java
@@ -2,6 +2,7 @@ package de.webcodr.chartrack.CharacterViewActivity;
 
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,12 @@ public class HeroAdapter extends ArrayAdapter<Hero> {
     private Context context;
     private ArrayList<Hero> heroList;
 
+    static class ViewHolder {
+        public TextView characterName;
+        public TextView characterLevel;
+        public TextView className;
+    }
+
     public HeroAdapter(Context context, ArrayList<Hero> heroList) {
         super(context, R.layout.hero_view, heroList);
         this.context = context;
@@ -25,17 +32,38 @@ public class HeroAdapter extends ArrayAdapter<Hero> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View heroView = inflater.inflate(R.layout.hero_view, parent, false);
-        TextView txtCharacterName = (TextView) heroView.findViewById(R.id.txtCharacterName);
-        TextView txtClassName = (TextView) heroView.findViewById(R.id.txtClassName);
-        TextView txtCharacterLevel = (TextView) heroView.findViewById(R.id.txtCharacterLevel);
-        Hero hero = heroList.get(position);
-
-        txtCharacterName.setText(hero.getName());
-        txtCharacterLevel.setText("Level " + hero.getLevel().toString());
-        txtClassName.setText(hero.getClassName());
+        View heroView = getHeroView(convertView, parent);
+        setCharacterInformation((ViewHolder) heroView.getTag(), heroList.get(position));
 
         return heroView;
+    }
+
+    private View getHeroView(View convertView, ViewGroup parent) {
+        View heroView = convertView;
+
+        if (heroView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            heroView = inflater.inflate(R.layout.hero_view, parent, false);
+            ViewHolder viewHolder = getViewHolder(heroView);
+
+            heroView.setTag(viewHolder);
+        }
+
+        return heroView;
+    }
+
+    private ViewHolder getViewHolder(View heroView) {
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.characterName = (TextView) heroView.findViewById(R.id.txtCharacterName);
+        viewHolder.characterLevel = (TextView) heroView.findViewById(R.id.txtCharacterLevel);
+        viewHolder.className = (TextView) heroView.findViewById(R.id.txtClassName);
+
+        return viewHolder;
+    }
+
+    private void setCharacterInformation(ViewHolder viewHolder, Hero hero) {
+        viewHolder.characterName.setText(hero.getName());
+        viewHolder.characterLevel.setText("Level " + hero.getLevel().toString());
+        viewHolder.className.setText(hero.getClassName());
     }
 }
